dca72973dc639bda566852e89a8c223c3d491621
Bruno Barros <barrosbrunofelipe@gmail.com>
2022-04-27 22:11:31 -0300

    feat: Criando conexão com o banco de dados, instalando biblioteca joi e dayjs, realizando o armazenamento dos dados do participante assim como as mensagens de status

--- index.js
+++ index.js
@@ -1,9 +1,60 @@
-import express, {json} from "express";
+import express, { json } from "express";
 import cors from "cors";
 import chalk from "chalk";
+import dayjs from "dayjs";
+import joi from "joi";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
 
+dotenv.config();
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+// ...
 const app = express();
 app.use(cors());
 app.use(json());
 
-app.listen(5000, console.log(chalk.bold.green("Servidor rodando na porta 5000")));
+app.post("/participants", async (req, res) => {
+    const participante = joi.object({
+        name: joi.string()
+    })
+    const validacao = participante.validate(req.body);
+
+    if (validacao.error) {
+        console.log(validacao.error.details)
+        res.status(422).send("Envia um objeto no formarto: { name: João}");
+        return;
+    }
+
+    const { name } = req.body;
+
+    try {
+        await mongoClient.connect();
+        const dbBatePapo = mongoClient.db("batepapo");
+        const participantesCollection = dbBatePapo.collection("participantes");
+        const temParticipante = await participantesCollection.findOne({ name: name });
+        if (temParticipante) {
+            console.log(temParticipante);
+            res.status(409).send("Nome de usuário já cadastrado");
+            return;
+        } else {
+            await participantesCollection.insertOne({ name: name, lastStatus: Date.now() })
+            const mensagensCollection = dbBatePapo.collection("mensagens");
+            await mensagensCollection.insertOne({
+                from: name, 
+                to: 'Todos',
+                text: 'entra na sala...',
+                type: 'status',
+                time: dayjs().locale('pt-br').format('hh:mm:ss')
+            })
+            res.sendStatus(201);
+            mongoClient.close();
+        }
+    } catch (e) {
+        console.log(e);
+        res.sendStatus(500);
+        mongoClient.close();
+    }
+});
+
+
+app.listen(5000, console.log(chalk.bold.yellow("Servidor rodando na porta 5000")));
